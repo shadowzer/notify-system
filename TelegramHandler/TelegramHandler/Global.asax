230a37be4d974a595f504cc9dfb765886ad172b5
shad <shadow.mr96@gmail.com>
2017-10-30 21:47:48 +0500

    Initial TelegramHandler commit

---
+++ TelegramHandler/TelegramHandler/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TelegramHandler.WebApiApplication" Language="C#" %>
---
+++ TelegramHandler/TelegramHandler/Models/TelegramConvert.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TelegramHandler.Models
+{
+	public class TelegramConvert : IMessageConverter
+	{
+		public object Convert(Message message)
+		{
+			return new TelegramMessage
+			{
+				chat_id = message.Id,
+				text = message.Text
+			}; ;
+		}
+	}
+}
