4d0bcf937917ff805a015381d7f7633ae8b4d591
Volginn <nikitavolgin97@mail.ru>
2017-10-31 13:36:24 +0500

    init ViberHandler

---
+++ ViberHandler/ViberHandler/Models/ViberMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ViberHandler.Models
+{
+	public class ViberMessage
+	{
+		public string chat_id { get; set; }
+		public string text { get; set; }
+
+		public override string ToString()
+		{
+			return "Id: " + chat_id + ", Text: " + text;
+		}
+	}
+}
