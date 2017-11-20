ed70de6e50964209bd37df091272297f1cf0047a
Volginn <nikitavolgin97@mail.ru>
2017-11-20 23:17:53 +0500

    Revert "change Api project on console , added RabbitMQ queue"
    This reverts commit db9645eaad2355113a003f6a83a0df1362c1c4b9.

---
+++ ViberHandler/ViberHandler/Models/IMessageConverter.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace ViberHandler.Models
+{
+	public interface IMessageConverter
+	{
+		object Convert(Message message);
+	}
+}
---
+++ ViberHandler/ViberHandler/Models/ViberConvert.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ViberHandler.Models
+{
+	public class ViberConvert : IMessageConverter
+	{
+		public object Convert(Message message)
+		{
+			return new ViberMessage
+			{
+				chat_id = message.Id,
+				text = message.Text
+			}; ;
+		}
+	}
+}
