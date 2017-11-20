ad9942d1ebead4012fb8b586c0d1a2246b20dd5a
Volginn <nikitavolgin97@mail.ru>
2017-11-20 23:20:08 +0500

    changed API project to console,added rabbitmq queue

---
+++ Viber-handler/Viber-handler/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net.Http;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Web.Helpers;
+using Newtonsoft.Json;
+
+namespace Viber_handler
+{
+    class Worker
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "messages",
+                    type: "direct");
+                var queueName = channel.QueueDeclare().QueueName;
+
+                channel.QueueBind(queue: queueName,
+                    exchange: "messages",
+                    routingKey: "viber");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                     using (var client = new HttpClient())
+                    {
+                            client.BaseAddress = new Uri("https://chatapi.viber.com/pa/send_message");//TODO cut url to config
+                            var resp = client.PostAsync("", new StringContent(new ViberMessage(message).ToString(), Encoding.UTF8, "application/json"));
+                            Console.WriteLine(resp.Result.Content.ReadAsStringAsync().Result);
+                    }
+                    Console.WriteLine(new ViberMessage(message).ToString());
+                };
+                channel.BasicConsume(queue: queueName,
+                    noAck: true,
+                    consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Viber-handler/Viber-handler/ViberMessage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Viber_handler
+{
+    public class ViberMessage
+    {
+
+        public string Id { get; set; }
+        public string Text { get; set; }
+
+        public ViberMessage(string mess)
+        {
+            Id = mess.Substring(mess.IndexOf(':') + 1, mess.IndexOf("Text", StringComparison.OrdinalIgnoreCase) - mess.IndexOf(':')-3);
+            Text = mess.Substring(mess.IndexOf("text", StringComparison.OrdinalIgnoreCase)+5);
+        }
+
+        public override string ToString()
+        {
+            return "{\"auth_token\" :" + "\"46d1678012e7d48c-4a411369fcd8263d-122e41e707bdf322\" " + ",\"receiver\": \"" + Id + "\", \"text\": \"" + Text + "\",\"type\" : \"text\"}";// TODO:chenge sender name 
+        }
+    }
+}
