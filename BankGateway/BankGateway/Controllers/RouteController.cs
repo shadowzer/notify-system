cc986edde9eb3522723649b6743c64d4a7f2a346
shad <shadow.mr96@gmail.com>
2017-12-10 19:03:40 +0500

    initial bank gateway commit

---
+++ BankGateway/BankGateway/Controllers/RouteController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using BankGateway.Models;
+using Newtonsoft.Json;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+
+namespace BankGateway.Controllers
+{
+	public class RouteController : ApiController
+	{
+		[HttpPost]
+		[Route("sendMessage")]
+		public string SendMessage(ServiceMessage serviceMessage)
+		{
+			switch (serviceMessage.Service.ToLower())
+			{
+				case "telegram":
+					using (var telegramChannel = RabbitClient.Instance.Connection.CreateModel())
+					{
+						telegramChannel.ExchangeDeclare(exchange: "notifyMessages",
+							type: "direct");
+						var telegramQueueName = telegramChannel.QueueDeclare().QueueName;
+						telegramChannel.QueueBind(queue: telegramQueueName,
+							exchange: "notifyMessages",
+							routingKey: "telegramPending");
+
+						telegramChannel.BasicPublish(exchange: "notifyMessages",
+							routingKey: "telegramPending",
+							body: Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(serviceMessage.Message))
+						);
+						return JsonConvert.SerializeObject("Message was queued for processing to Telegram.");
+					}
+				case "viber":
+					using (var viberChannel = RabbitClient.Instance.Connection.CreateModel())
+					{
+						viberChannel.ExchangeDeclare(exchange: "notifyMessages",
+							type: "direct");
+						var viberQueueName = viberChannel.QueueDeclare().QueueName;
+						viberChannel.QueueBind(queue: viberQueueName,
+							exchange: "notifyMessages",
+							routingKey: "viberPending");
+
+						viberChannel.BasicPublish(exchange: "notifyMessages",
+							routingKey: "viberPending",
+							body: Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(serviceMessage.Message))
+						);
+						return JsonConvert.SerializeObject("Message was queued for processing to Viber.");
+					}
+				default:
+					return JsonConvert.SerializeObject("Service \"" + serviceMessage.Service +
+					                                   "\" is currently not supported. Supported services: Telegram, Viber.");
+			}
+		}
+	}
+}
---
+++ BankGateway/BankGateway/Models/ServiceMessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BankGateway.Models
+{
+	public class ServiceMessage
+	{
+		public string Service { get; set; }
+		public Message Message { get; set; }
+	}
+}
